6137f1c4d99f63f8fe7e12b799aa20cf4e5d6472
Keerthana <keerthanathirumoorthy20>
2023-06-20 12:49:51 +0530

    guess

---
+++ script.js
@@ -0,0 +1,28 @@
+// select the Element
+// input
+const numberEl = document.getElementById("number");
+const btnGuessEl = document.getElementById("btn_guess");
+const btnPlayEl = document.getElementById("btn_play");
+
+// output
+const scoreEl = document.getElementById("score");
+const highscoreEl = document.getElementById("highscore");
+const msgEl = document.getElementById("msg");
+
+// solution
+const getRandomNumber = function () {
+  return Math.trunc(Math.random () * 100 + 1)
+}
+
+const randomNumber = getRandomNumber();
+
+// Button guess event
+btnguess.addEventListener('click', function () {
+  const userInput = guessInputEl.value;
+  console.log(userInput);
+})
+
+
+
+
+
